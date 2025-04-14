39bee5c0d48d2de820a5b409dd4755a4f10a1f38
Davi RJ <daviribeirodjust007.28@gmail.com>
2025-04-14 19:40:27 -0300

    ajuste finais

--- trabalho_FDS/app.js
+++ trabalho_FDS/app.js
@@ -1,16 +1,15 @@
 let bt = document.querySelector('#bt');
-//let msg = document.querySelector('#msg')
+let msg = document.querySelector('#msg')
 
-bt.addEventListener('click', () => {
-    saudacao();
-});
+bt.addEventListener('click', saudacao );
     function saudacao(){
         var nome = document.getElementById('nome').value;
         const valorInput = document.getElementById('nome').value;
             if(nome === "")
                 alert ('digite um nome');        
             else {
-                localStorage.setItem('nomeVisitante',valorInput);
-                window.location.href = "pagina1.html";
-            }
-    }
+                msg.innerHTML = valorInput+", "+'seja bem-vindo';
+                setTimeout(() => msg.remove(),2000);
+                setTimeout(() =>{window.location.href = "pagina1.html"}, 2000);                                      
+        }   
+    };
--- trabalho_FDS/pagina1.js
+++
@@ -1,6 +0,0 @@
-const valorInput = localStorage.getItem('nomeVisitante');
-const msg = document.getElementById('msg');
-    if (valorInput) {
-    msg.innerHTML = valorInput+", "+'seja bem-vindo';
-    setTimeout(() => msg.remove (), 5000);
-}   
